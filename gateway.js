20a92fb27bb0a9b5a51d0fe8de4aef789bbdc5d6
ajnasa2285 <anujnasa2285@gmail.com>
2026-08-17 00:29:09 +0530

    Update gateway.js

--- gateway.js
+++ gateway.js
@@ -1,5 +1,5 @@
 const axios = require('axios');
-const config = require('../config');
+const config = require('./config');
 
 /**
  * Sends a text message back to a WhatsApp user via the configured gateway.
